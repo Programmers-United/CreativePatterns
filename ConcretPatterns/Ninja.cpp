74e322fd12be6bc9882e82dd228cd54942f9cfc3
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-16 21:02:52 -0300

    feat: Creating the Ninja ConcretPrototype

--- ConcretPatterns/Ninja.cpp
+++ ConcretPatterns/Ninja.cpp
@@ -1,5 +1,17 @@
-//
-// Created by Silva on 16/08/2024.
-//
-
 #include "Ninja.h"
+
+Ninja::Ninja() : name(""), description(""), skill("") {}
+
+Ninja::Ninja(string name, string description, string skill) : name(name), description(description), skill(skill) {}
+
+unique_ptr<Prototype<Ninja>> Ninja::clone() const {
+    return make_unique<Ninja>(*this);
+}
+
+void Ninja::info() const {
+    cout << "Name: " << name << "\nDescription: " << description << "\nSkill: " << skill << endl;
+}
+
+void Ninja::setNameClone(string nameClone) {
+    name = nameClone;
+}
--- ConcretPatterns/Ninja.h
+++ ConcretPatterns/Ninja.h
@@ -1,16 +1,26 @@
-//
-// Created by Silva on 16/08/2024.
-//
-
 #ifndef NINJA_H
 #define NINJA_H
 
+#include "../pattern/Prototype.h"
+#include <string>
+#include <memory>
 
+using namespace std;
 
-class Ninja {
+class Ninja : public Prototype<Ninja> {
+protected:
+    string name;
+    string description;
+    string skill;
 
+public:
+    Ninja();
+    Ninja(string name, string description, string skill);
+
+    unique_ptr<Prototype<Ninja>> clone() const override;
+    void info() const override;
+
+    void setNameClone(string nameClone);
 };
-
-
 
 #endif //NINJA_H
