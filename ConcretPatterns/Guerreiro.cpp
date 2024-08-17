657ce0bd883a4c0a7fd9a7292b3641de50264a7f
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-16 21:01:25 -0300

    feat: Creating the Warrior ConcretPrototype

--- ConcretPatterns/Guerreiro.cpp
+++ ConcretPatterns/Guerreiro.cpp
@@ -1,5 +1,20 @@
-//
-// Created by Silva on 16/08/2024.
-//
-
 #include "Guerreiro.h"
+using namespace std;
+
+Guerreiro::Guerreiro() : name(""), description("") , power(""){}
+
+Guerreiro::Guerreiro(string name, string description, string power) : name(name), description(description), power(power){}
+
+unique_ptr<Prototype<Guerreiro> > Guerreiro::clone() const {
+    return make_unique<Guerreiro>(*this);
+}
+
+void Guerreiro::info() const {
+    cout << "Name: " << this->name <<  "\nDescription: " << this->description <<  "\nPower: " << this->power << endl;
+}
+
+void Guerreiro::setNameClone(string nameClone) {
+    name = nameClone;
+}
+
+
--- ConcretPatterns/Guerreiro.h
+++ ConcretPatterns/Guerreiro.h
@@ -1,16 +1,23 @@
-//
-// Created by Silva on 16/08/2024.
-//
-
 #ifndef GUERREIRO_H
 #define GUERREIRO_H
 
+#include "../pattern/Prototype.h"
+using namespace std;
 
+class Guerreiro: public Prototype<Guerreiro>{
+    protected:
+        string name;
+        string description;
+        string power;
 
-class Guerreiro {
+    public:
+        Guerreiro();
+        Guerreiro(string name, string description, string power);
 
+        unique_ptr<Prototype<Guerreiro>> clone() const override;
+        void info() const override;
+
+        void setNameClone(string nameClone);
 };
-
-
 
 #endif //GUERREIRO_H
