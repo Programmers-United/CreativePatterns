1f02e117400722f9e13270b9e294b0b429d97fe6
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-16 21:02:29 -0300

    feat: Creating the Mago ConcretPrototype

--- ConcretPatterns/Mago.cpp
+++ ConcretPatterns/Mago.cpp
@@ -1,5 +1,18 @@
-//
-// Created by Silva on 16/08/2024.
-//
-
 #include "Mago.h"
+
+Mago::Mago() {}
+
+Mago::Mago(string name, string description, string typeMago) : name(name), description(description), typeMago(typeMago) {}
+
+
+unique_ptr<Prototype<Mago> > Mago::clone() const {
+    return make_unique<Mago>(*this);
+}
+
+void Mago::info() const {
+    cout << "Mago: " << name << "\nDescription: " << description << "\nTipo de mago: " << typeMago << endl;
+}
+
+void Mago::setNameClone(string nameClone) {
+    name = nameClone;
+}
--- ConcretPatterns/Mago.h
+++ ConcretPatterns/Mago.h
@@ -1,16 +1,22 @@
-//
-// Created by Silva on 16/08/2024.
-//
-
 #ifndef MAGO_H
 #define MAGO_H
 
+#include "../pattern/Prototype.h"
+using namespace std;
 
+class Mago : public Prototype<Mago>{
+    protected:
+        string name;
+        string description;
+        string typeMago;
 
-class Mago {
+    public:
+    Mago();
+    Mago(string name, string description, string typeMago);
+    unique_ptr<Prototype<Mago>> clone() const override;
+    void info() const override;
 
+    void setNameClone(string nameClone);
 };
-
-
 
 #endif //MAGO_H
