97ba52ac6a55d48b57b457e0455c7e2c129f22e6
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-16 21:04:49 -0300

    feat: Creating the main of the algorithm

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,41 @@
-//
-// Created by Silva on 16/08/2024.
-//
+#include <iostream>
+using namespace std;
 
-#include "Main.h"
+#include "Game/Game.h"
+
+int main() {
+    Game jogo; //Instanciando o game
+    int choice; //Escolha de menu
+
+    while (true) {
+        cout << "\nMenu:\n";
+        cout << "1. Criar Personagens\n";
+        cout << "2. Clonar Personagens\n";
+        cout << "3. Listar Personagens Originais\n";
+        cout << "4. Listar Personagens Clonados\n";
+        cout << "5. Sair\n";
+        cout << "Escolha uma opcao: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                jogo.criarPersonagens();
+            break;
+            case 2:
+                jogo.clonarPersonagens();
+            break;
+            case 3:
+                jogo.listarPersonagensOriginais();
+            break;
+            case 4:
+                jogo.listarPersonagensClonados();
+            break;
+            case 5:
+                cout << "\nSaindo do programa." << endl;
+            return 0;
+            default:
+                cout << "\nOpcao invalida. Tente novamente." << endl;
+            break;
+        }
+    }
+}
