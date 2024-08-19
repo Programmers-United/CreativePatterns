e26b53c5a5509a3b9a2b7c20d4c6427ad36acc63
7-Dodi <douglassilvaaraujo78@gmail.com>
2024-08-19 18:49:33 -0300

    feat: Creating the EletricEngine concrete service

--- Injection-Dependency/Concret-Service/EletricEngine.h
+++ Injection-Dependency/Concret-Service/EletricEngine.h
@@ -5,4 +5,18 @@
 #ifndef ELETRICENGINE_H
 #define ELETRICENGINE_H
 
+#include "../Service/IEngine.h"
+#include <iostream>
+using namespace std;
+
+class EletricEngine: public IEngine {
+    void start() const override {
+        cout << "\nMotor eletrico em uso!" << endl;
+    }
+
+    string getType() const override {
+        return "Motor eletrico";
+    }
+};
+
 #endif //ELETRICENGINE_H
